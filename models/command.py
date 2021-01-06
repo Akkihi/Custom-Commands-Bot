50585c283e2b2928cc5d9bdc388aa51ba31fb922
io1zl <io1zl@protonmail.com>
2021-01-06 06:21:38 +0200

    change field name in command table

--- models/command.py
+++ models/command.py
@@ -7,7 +7,7 @@ from .chat import Chat
 
 class Command(BaseModel):
     created_by = ForeignKeyField(User, backref='commands', on_delete='CASCADE')
-    chat = ForeignKeyField(Chat, backref='commands', on_delete='CASCADE')
+    to_chat = ForeignKeyField(Chat, backref='commands', on_delete='CASCADE')
     text = TextField(null=True)
     media_msg_id = BigIntegerField(null=True)
     is_inline = BooleanField(default=False)
