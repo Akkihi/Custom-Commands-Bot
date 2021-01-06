7d67e6f9d3b2ae762c736e42e10545003e576c1b
io1zl <io1zl@protonmail.com>
2021-01-06 06:01:38 +0200

    edit database structure

--- models/chat.py
+++ models/chat.py
@@ -6,4 +6,5 @@ from .base_model import BaseModel
 class Chat(BaseModel):
     telegram_id = BigIntegerField(unique=True)
     title = TextField(null=True)
+    link = TextField(null=True)
     premium_to = DateTimeField()
