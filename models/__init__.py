0e9f838a38191cd1a179e387912fb158daeedf2f
io1zl <io1zl@protonmail.com>
2021-01-06 06:38:05 +0200

    added save adapter

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 import os
+from typing import Union
 
+import aiogram
 from peewee import *
 
 from .base_model import BaseModel, DATABASE
@@ -16,3 +18,45 @@ if not os.path.exists(DATABASE):
         Command,
     ])
 
+
+def save_user(user: aiogram.types.User):
+    db_user, created = User.get_or_create(telegram_id=user.id)
+    db_user.first_name = user.first_name
+    db_user.last_name = user.last_name
+    db_user.username = user.username
+    db_user.save()
+
+    return db_user, created
+
+
+def save_chat(chat: aiogram.types.Chat):
+    db_chat, created = Chat.get_or_create(telgram_id=chat.id)
+    db_chat.title = chat.title
+    db_chat.link = chat.invite_link
+    db_chat.save()
+
+    return db_chat, created
+
+
+def save_command(text,
+                 media: Union[aiogram.types.Document,
+                              aiogram.types.PhotoSize,
+                              aiogram.types.Audio],
+                 created_by: aiogram.types.User,
+                 to_chat: aiogram.types.Chat = None,
+                 is_inline=False,
+                 is_reply=False):
+    db_user, created = save_user(created_by)
+
+    if to_chat:
+        db_chat, created = save_chat(to_chat)
+    else:
+        db_chat = None
+
+    db_command, created = Command.get_or_create(created_by=db_user,
+                                                to_chat=db_chat,
+                                                text=text,
+                                                is_inline=is_inline,
+                                                is_reply=is_reply)
+
+    return db_command, created
